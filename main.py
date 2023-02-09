494a9d476ced927b8e1a387c02abbad7baef5648
lennonay <lennon.auyeung@me.com>
2023-02-08 16:26:02 -0800

    modify main

--- main.py
+++ main.py
@@ -9,16 +9,17 @@ if __name__ == "__main__":
     today = datetime.today().strftime('%Y-%m-%d')
 
     start_game_id = 1018603 + 1
-    end_game_id = 1018603 + 11
+    end_game_id = 1018603 + 537
 
     game_info = game_scrape(start_game_id, end_game_id)
 
-    #game_info.to_csv('data/whl_game_info_{start}_to_{end}.csv'.format(start = start_game_id, end = end_game_id-1),index=False)
+    game_info.to_csv('data/whl_game_info_{start}_to_{end}.csv'.format(start = start_game_id, end = end_game_id-1),index=False)
 
     #roster_df = pd.read_csv('data/roster_2023-02-05.csv')
 
     roster_df = roster()
-    #roster_df.to_csv('data/roster_{date}.csv'.format(date = today),index=False)
+    
+    roster_df.to_csv('data/roster_{date}.csv'.format(date = today),index=False)
 
     game_info_dob = pd.merge(game_info,roster_df, on = ['player_id','first_name','last_name'], how = 'left')
 
