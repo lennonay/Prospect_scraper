7f5d886c1ae797eb56b8660f730f256fba3fad5c
lennonay <lennon.auyeung@me.com>
2023-04-01 16:28:53 -0700

    modify message

--- schedule.py
+++ schedule.py
@@ -2,10 +2,13 @@ import requests
 import pandas as pd
 from datetime import datetime
 
-def schedule(leauge = 'whl', key = '41b145a848f4bd67', season = '279'):
+def schedule(league_info):
     
+    finished = 0
+    no_games = 0
+
     url = 'https://lscluster.hockeytech.com/feed/?feed=modulekit&view=schedule&key={key}&fmt=json&client_code={league}&lang=en&season_id={season}team_id=&league_code=&fmt=json'.format(
-        key = key, league = leauge, season = season)
+        key = league_info['key'], league = league_info['league'], season = league_info['season'])
 
     response = requests.get(url)
     schedule = response.json()['SiteKit']['Schedule']
@@ -14,25 +17,31 @@ def schedule(leauge = 'whl', key = '41b145a848f4bd67', season = '279'):
 
     #check if there is any games in the schedule
     if (len(schedule_df) == 0):
-        print('The season has not started yet.')
+        print('The season {} has not started yet'.format(league_info['name']))
         return
     
     today = datetime.today().strftime('%Y-%m-%d')
 
     #check if the season has finished
     if (today > schedule_df.iloc[-1]['date_played']):
-        print('The season has concluded')
+        print('The season {} has concluded'.format(league_info['name']))
+        finished = 1
 
     fin_games = schedule_df[schedule_df['final'] == '1']
 
     #check if there is any finished games
     if (len(fin_games) == 0):
-        print('No games has finished yet.')
+        print('No games in {} has finished yet'.format(league_info['name']))
+        no_games = 1
         return
+    
+    if finished !=1 and no_games != 1:
+        print('The season {} is underway'.format(league_info['name']))
 
     game_id = fin_games['game_id'].tolist()
 
     return game_id
 
 if __name__ == "__main__":
-   print(type(schedule()))
+   league_info = pd.read_csv('data/league_info.csv')
+   print((schedule(league_info.iloc[1])))
